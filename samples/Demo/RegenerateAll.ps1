7e4370eda99956712901d19a2d314907bbee1458
ostat <max@ostat.com>
2021-09-11 13:26:05 +1000

    RegenerateAll.ps1 no longer not relies working folder. Set default working folder for code gen projects.

--- samples/Demo/RegenerateAll.ps1
+++ samples/Demo/RegenerateAll.ps1
@@ -1,5 +1,23 @@
-Get-ChildItem -LiteralPath . -Filter 'generated' -Directory -Recurse | Get-ChildItem | Remove-Item -Recurse -Force
-cd Beef.Demo.CodeGen
-dotnet run all
-cd ../Beef.Demo.Database
-dotnet run all
+###############################################################
+#
+# Removes all generated files then re-generates them running
+# CodeGen 
+#     Beef.Demo.CodeGen/Beef.Demo.CodeGen.csproj
+# Database CodeGen
+#     Beef.Demo.Database/Beef.Demo.Database.csproj
+#
+###############################################################
+
+#Get the current script path
+$path = $PSScriptRoot
+
+Write-Host "Removing all generated files path:'$path'" -ForegroundColor Green
+Get-ChildItem -LiteralPath $path -Filter 'generated' -Directory -Recurse | Get-ChildItem | Remove-Item -Recurse -Force
+
+$CodeGenPath = (Join-Path $path 'Beef.Demo.CodeGen/Beef.Demo.CodeGen.csproj')
+Write-Host "Running Beef.Demo.CodeGen path:'$CodeGenPath'" -ForegroundColor Green
+dotnet run all --project $CodeGenPath
+
+$DatabasePath = (Join-Path $path 'Beef.Demo.Database/Beef.Demo.Database.csproj')
+Write-Host "Running Beef.Demo.Database path:'$DatabasePath'"-ForegroundColor Green
+dotnet run all --project $DatabasePath
