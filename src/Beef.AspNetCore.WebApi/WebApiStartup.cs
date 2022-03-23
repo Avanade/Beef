ccd128b88b61b283a06bde85fd80b1b02e510498
Piotr <karpik.pl+nospam@gmail.com>
2022-03-23 11:48:01 -0400

    updated code comment

--- src/Beef.AspNetCore.WebApi/WebApiStartup.cs
+++ src/Beef.AspNetCore.WebApi/WebApiStartup.cs
@@ -22,7 +22,7 @@ namespace Beef.AspNetCore.WebApi
         /// </summary>
         /// <typeparam name="TStartup">The API startup <see cref="Type"/>.</typeparam>
         /// <param name="args">The command line args.</param>
-        /// <param name="environmentVariablePrefix">The prefix that the environment variables must start with.</param>
+        /// <param name="environmentVariablePrefix">The prefix that the environment variables must start with (will automatically add a trailing underscore where not supplied).</param>
         /// <returns>The <see cref="IWebHost"/>.</returns>
         public static IWebHostBuilder CreateWebHost<TStartup>(string[] args, string? environmentVariablePrefix = null) where TStartup : class =>
             WebHost.CreateDefaultBuilder(args)
@@ -34,7 +34,7 @@ namespace Beef.AspNetCore.WebApi
         /// </summary>
         /// <typeparam name="TStartup">The API startup <see cref="Type"/>.</typeparam>
         /// <param name="args">The command line args.</param>
-        /// <param name="environmentVariablePrefix">The prefix that the environment variables must start with.</param>
+        /// <param name="environmentVariablePrefix">The prefix that the environment variables must start with (will automatically add a trailing underscore where not supplied).</param>
         /// <returns>The <see cref="IWebHost"/>.</returns>
         public static IWebHost BuildWebHost<TStartup>(string[] args, string? environmentVariablePrefix = null) where TStartup : class =>
             CreateWebHost<TStartup>(args, environmentVariablePrefix).Build();
