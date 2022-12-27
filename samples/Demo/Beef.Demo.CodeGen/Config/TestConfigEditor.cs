23429df16659002ff2bb0b2b62bfb3da162dfb3b
Kota Nishizaki <kota.nishizaki@avanade.com>
2022-12-28 07:24:33 +0900

    Add conditional branching based on the existence of an ExtraProperty item and an item value to the template in Demo sample
    Signed-off-by: Kota Nishizaki <kota.nishizaki@avanade.com>

--- samples/Demo/Beef.Demo.CodeGen/Config/TestConfigEditor.cs
+++ samples/Demo/Beef.Demo.CodeGen/Config/TestConfigEditor.cs
@@ -15,6 +15,15 @@ namespace Beef.Demo.CodeGen.Config
                 // Look for the additional property added in the configuration file.
                 if (e.TryGetExtraProperty("TestCodeGen", out JValue val) && val.ToObject<bool>())
                     e.CustomProperties["TestExtra"] = $"XXX.{e.GetExtraProperty<JValue>("TestExtra")}.XXX"; // Add a new custom property that can be referenced in the template.
+
+                // In order to test the additional properties existence in the template,
+                // store whether the specified additional properties exist in the configuration file.
+                foreach (var name in new string[] { "TestExist", "TestNotExist" })
+                {
+                    bool defined = e.ExtraProperties?.ContainsKey(name) ?? false;
+                    e.CustomProperties[$"{name}-Defined"] = defined;
+                    e.CustomProperties[name] = defined ? e.GetExtraProperty<JValue>(name).Value : null;
+                }
             }
 
             return Task.CompletedTask;
